a802b9c5771162dfae6ed2790ed51115daafea5c
Luis Fernandez <luisfsangil@gmail.com>
2024-04-30 10:16:01 +0200

    Exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,39 @@
 import { useState } from 'react'
 
+const GiveFeedbakc = ({updateGoods, updateNeutrals, updateBads}) => {
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={updateGoods}>good</button>
+      <button onClick={updateNeutrals}>neutral</button>
+      <button onClick={updateBads}>bad</button>
+    </>
+  )
+}
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  )
+}
 const App = () => {
-  // save clicks of each button to its own state
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const updateGoods = () => setGood(g => g+1)
+  const updateNeutrals = () => setNeutral(n => n + 1)
+  const updateBads = () => setBad(b => b + 1)
+
   return (
     <div>
-      code here
+      <GiveFeedbakc updateGoods={updateGoods} updateNeutrals={updateNeutrals} updateBads={updateBads}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
