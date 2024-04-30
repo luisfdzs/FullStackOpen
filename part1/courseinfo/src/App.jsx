ab818b1d22269115562d22898133a7ad194b1577
Luis Fernandez <luisfsangil@gmail.com>
2024-04-30 17:37:09 +0200

    Exercise 2.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,14 +1,5 @@
-const Header = ({course}) => <h2>{course.name}</h2>
-const Part = ({name, exercises}) => <p>{name} {exercises}</p>
-const Content = ({course}) => <>{course.parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises}/>)}</>
-const Total = ({course}) => <p><strong>total of {course.parts.reduce((total, part) => total + part.exercises, 0)} exercises</strong></p>
-const Course = ({course}) => (
-  <>
-    <Header course={course}/>
-    <Content course={course}/>
-    <Total course={course}/>
-  </>
-)
+import { Course } from "./components/Course.jsx"
+
 const App = () => {
   const courses = [
     {
