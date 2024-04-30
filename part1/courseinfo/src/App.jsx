1836bb514eb04de34e6e3ea8ebd219ae4bc9d9e2
Luis Fernandez <luisfsangil@gmail.com>
2024-04-30 17:27:26 +0200

    Exercise 2.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,7 +2,13 @@ const Header = ({course}) => <h1>{course.name}</h1>
 const Part = ({name, exercises}) => <p>{name} {exercises}</p>
 const Content = ({course}) => <>{course.parts.map(part => <Part key={part.name} name={part.name} exercises={part.exercises}/>)}</>
 const Total = ({course}) => <p>Number of exercises {course.parts.reduce((total, part) => total + part.exercises, 0)}</p>
-
+const Course = ({course}) => (
+  <>
+    <Header course={course}/>
+    <Content course={course}/>
+    {/* <Total course={course}/> */}
+  </>
+)
 const App = () => {
     const course = {
         name: 'Half Stack application development',
@@ -16,15 +22,12 @@ const App = () => {
         },{
             name: 'State of a component',
             exercises: 14
-        }]
+        },{
+          name: 'LastPart!!',
+          exercises: 10717
+      }]
     }
-    return (
-      <div>
-        <Header course={course}/>
-        <Content course={course}/>
-        <Total course={course}/>
-      </div>
-    )
+    return <Course course={course}/>
   }
   
   export default App
