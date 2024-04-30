2386861f835a2965ddff468b68a9e34c353a4c13
Luis Fernandez <luisfsangil@gmail.com>
2024-04-30 10:20:19 +0200

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,17 +11,23 @@ const GiveFeedbakc = ({updateGoods, updateNeutrals, updateBads}) => {
   )
 }
 const Statistics = ({good, neutral, bad}) => {
+  const all = good+neutral+bad
+  const average = (good-bad)/all
+  const positive = good/all*100
   return (
     <>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average ? average : 0}</p>
+      <p>positive {positive ? positive : 0}%</p>
     </>
   )
 }
 const App = () => {
-  
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
