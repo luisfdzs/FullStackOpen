c36142d97b540916e7a95c944e5b8de7e0aa0677
Luis Fernandez <luisfsangil@gmail.com>
2024-04-30 18:24:05 +0200

    Exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,34 +2,37 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+       name: 'Arto Hellas',
+       number: '040-1234567'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const updateNewName = (event) => setNewName(event.target.value)
+  const updateNewNumber = (event) => setNewNumber(event.target.value)
   const addNewPerson = (event) => {
     event.preventDefault()
     const alreadyExists = persons.some(person => person.name.toLowerCase() === newName.toLowerCase())
     if (alreadyExists) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons([...persons, {name: newName}])
+      setPersons([...persons, {name: newName, number: newNumber}])
     }
     setNewName('')
+    setNewNumber('')
   }
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
-        <div>
-          name: <input onChange={updateNewName} value={newName}/>
-        </div>
-        <div>
-          <button onClick={addNewPerson}>add</button>
-        </div>
+        <p>name: <input onChange={updateNewName} value={newName}/></p>
+        <p>number: <input onChange={updateNewNumber} value={newNumber}/></p>
+        <button onClick={addNewPerson}>add</button>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
